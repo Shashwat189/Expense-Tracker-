2c90b8870df1c3cbb252234f9e3f276af3d15112
Shashwat18 <shashwatkaul891@gmail.com>
2026-05-14 21:28:10 +0530

    convert app to vite spa

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -6,12 +6,8 @@ import { getRouter } from "./router";
 
 const router = getRouter();
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
