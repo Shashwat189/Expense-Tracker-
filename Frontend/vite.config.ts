da2ec7814a686665dfd99b9c6dda7fa5511d1af9
Shashwat18 <shashwatkaul891@gmail.com>
2026-05-14 23:07:01 +0530

    fix tanstack router generation

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    tanstackRouter(),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
